0c8186ef2cf026404613bf1d13bd6d17fd930236
Gideon Peters <gideonpeters85@gmail.com>
2019-01-12 02:00:10 +0100

    created 30 articles created articlecontroller

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,9 +2,9 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Article::class, function (Faker $faker) {
     return [
-        'title' => $faker->texxt(50),
+        'title' => $faker->text(50),
         'body'  => $faker->text(200)
     ];
 });
